57dffdbf0bb818be1cf2262a3f8dda674f01224e
RominaGonella <rominagonfur@gmail.com>
2022-06-26 23:16:13 +0000

    actualizo

--- src/app.py
+++ src/app.py
@@ -2,3 +2,6 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+https://drivendata.github.io/cookiecutter-data-science/
+
+##### PASAR EL CÓDIGO DEL NOTEBOOK PARA ACÁ!!!!!!!!!!!!!
